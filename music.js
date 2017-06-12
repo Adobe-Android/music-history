d3eca98d93cab1cf63925d0297c9cdce80572db5
Adobe-Android <tngamerboy@gmail.com>
2017-06-12 15:17:01 -0500

    initial setup

---
+++ music.js
@@ -0,0 +1,21 @@
+// Use JavaScript arrays, loops, and innerHTML to show the music you love.
+
+// Use JavaScript to create a list of songs in the index.html file for your Music History project. Download the songs.js file, which contains an array of strings with song information.
+
+// Add one song to the beginning and the end of the array.
+// Loop over the array, and remove any words or characters that obviously don't belong.
+// Find and replace the > character in each item with a - character.
+// Add each string to the DOM in index.html in the main content area.
+// Example output:
+
+// {Song name} by {Artist} on the album {Album}
+
+// {Song name} by {Artist} on the album {Album}
+
+var songs = [];
+
+songs[songs.length] = "Legs > by ZZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertramp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welcome to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironic > by Alanis Morisette on the album Jagged Little Pill";
