4afeaf0a5b4c0ee90ea1f8abeba88bf8f7f07e94
Adobe-Android <tngamerboy@gmail.com>
2017-06-12 16:47:56 -0500

    Slight CSS tweaks

--- music.js
+++ music.js
@@ -14,8 +14,13 @@
 
 var songs = [];
 
-songs[songs.length] = "Legs > by ZZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertramp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welcome to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironic > by Alanis Morisette on the album Jagged Little Pill";
+for (var i = 0; i < songs.length; i++) {
+ var songAdd = "Human-by Krewella on the album Ammunition" + songs[i] + "Warrior Concerto-by Glitch Mob on the album We Can Make the World Stop"
+ console.log("The array contains ", songAdd);
+}
+
+songs[songs.length] = "Legs-by ZZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song-by Supertramp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall-by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welcome to the Jungle-by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironic-by Alanis Morisette on the album Jagged Little Pill";
